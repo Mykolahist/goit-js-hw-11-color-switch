2df520c3b629aa2f1e6893ae1961f31577ba0d07
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2021-04-09 10:36:49 +0300

    final commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+let timerId = null;
+
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-action="start"]');
+const stopButton = document.querySelector('button[data-action="stop"]');
+
+startButton.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    changeBodyBackgroundColor();
+  }, 1000);
+  startButton.setAttribute('disabled', 'disabled');
+});
+console.log(startButton);
+
+stopButton.addEventListener('click', () => {
+  clearInterval(timerId);
+  startButton.removeAttribute('disabled');
+});
+console.log(stopButton);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const changeBodyBackgroundColor = () => {
+  body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+};
